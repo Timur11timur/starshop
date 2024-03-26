fc22b38c194004e43b5571d24a81520e0a121830
Timur Fralik <timur11timur@gmail.com>
2024-03-26 15:32:47 +0200

    Add Twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<h1>Hello</h1>');
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'Laravel',
+            'class' => 'framework',
+            'captain' => 'me',
+            'status' => 'loaded',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
 }
